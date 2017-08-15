b99769c23a1123d221f09711da71abb41e1979ab
Garati <any2miss@gmail.ru>
2017-08-15 10:15:59 +0300

    test commit

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, github!!!");
+            File.Create(".gitignore");
+
+        }
+    }
+}
